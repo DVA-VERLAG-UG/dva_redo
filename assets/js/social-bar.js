90bd3755c6c7579b0962d89bd03668f7587be13f
Navid Istanbullu <navido972@gmail.com>
2026-07-01 10:51:02 +0200

    sync

--- assets/js/social-bar.js
+++ assets/js/social-bar.js
@@ -3,21 +3,28 @@
 // Social media links configuration
 const SOCIAL_LINKS = {
   instagram: {
-    href: 'https://www.instagram.com/dvaydpublishing',
+    href: 'https://www.instagram.com/dvayd_verlag',
     label: 'Instagram',
     icon: `<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
       <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
     </svg>`,
   },
-  linkedin: {
-    href: 'https://www.linkedin.com/company/dvayd-publishing',
-    label: 'LinkedIn',
+  tiktok: {
+    href: 'https://www.tiktok.com/@dvayd.verlag',
+    label: 'TikTok',
     icon: `<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+      <path d="M16.6 5.82c-1.05-1.11-1.65-2.55-1.65-4.07h-3.03v14.79c0 1.79-1.45 3.24-3.24 3.24-1.79 0-3.24-1.45-3.24-3.24 0-1.79 1.45-3.24 3.24-3.24.29 0 .58.04.85.11V10.3c-.28-.04-.56-.06-.85-.06-3.48 0-6.29 2.81-6.29 6.29 0 3.48 2.81 6.29 6.29 6.29 3.48 0 6.29-2.81 6.29-6.29V9.28a9.13 9.13 0 0 0 5.3 1.7V7.94a5.85 5.85 0 0 1-3.67-2.12z"/>
+    </svg>`,
+  },
+  facebook: {
+    href: 'https://www.facebook.com/profile.php?id=61581852607603',
+    label: 'Facebook',
+    icon: `<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.988h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/>
     </svg>`,
   },
   email: {
-    href: 'mailto:contact@dvaydpublishing.com',
+    href: 'mailto:info@dva-yd.com',
     label: 'Email',
     icon: `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
@@ -61,11 +68,15 @@ function createSocialBar() {
   const lang = getCurrentLang();
 
   const socialLinksHTML = Object.entries(SOCIAL_LINKS)
-    .map(([, { href, label, icon }]) => `
-      <a href="${href}" target="_blank" rel="noopener noreferrer"
+    .map(([, { href, label, icon }]) => {
+      const isMailto = href.startsWith('mailto:');
+      const linkAttrs = isMailto ? '' : ' target="_blank" rel="noopener noreferrer"';
+      return `
+      <a href="${href}"${linkAttrs}
          class="social-link" aria-label="${label}" title="${label}">
         ${icon}
-      </a>`)
+      </a>`;
+    })
     .join('');
 
   const globeIcon = `<svg width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
